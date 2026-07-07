1d465702c3cda3d1e812bc6a543aa3db454a9353
Ethan Wen <75800128+wen-ethan@users.noreply.github.com>
2026-07-07 09:31:03 -0500

    initial app

---
+++ EchoAssist/EchoAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoAssist
+//
+//  Created by Ethan Wen on 7/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoAssist/EchoAssist/EchoAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoAssistApp.swift
+//  EchoAssist
+//
+//  Created by Ethan Wen on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
